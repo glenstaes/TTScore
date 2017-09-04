b4af29241b0e860ab3fd14ea495fe60890a95ec7
Glen Staes <staeseke@gmail.com>
2017-09-04 16:55:08 +0200

    feat(favorites): Navigate to selected team in favorites

--- app/teams/favorites/favorites.component.ts
+++ app/teams/favorites/favorites.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+let appSettings = require("application-settings");
+
 import { FavoritesService } from "./favorites.service";
 import { Team } from "../team.model";
+import { CURRENT_SELECTED_SEASON_KEY, CURRENT_SELECTED_CLUB_KEY, CURRENT_SELECTED_TEAM_KEY } from "../../settings/appsettingskeys";
 
 @Component({
     templateUrl: "teams/favorites/favorites.component.html"
@@ -17,12 +21,22 @@ export class FavoritesComponent implements OnInit {
     noFavorites = "Je hebt geen favorieten.";
 
     constructor(
-        private _favoritesService: FavoritesService
+        private _favoritesService: FavoritesService,
+        private _router: Router
     ){}
 
     ngOnInit(){
         this._favoritesService.getAll().subscribe((teams) => {
             this.favorites = teams;
         });
+    }
+
+    onTapFavoriteItem(eventData){
+        let favorite = this.favorites[eventData.index];
+        appSettings.setString(CURRENT_SELECTED_CLUB_KEY, favorite.parentClub.uniqueIndex);
+        appSettings.setNumber(CURRENT_SELECTED_SEASON_KEY, favorite.parentClub.seasonId);
+        appSettings.setString(CURRENT_SELECTED_TEAM_KEY, favorite.teamId);
+
+        this._router.navigate(["home"]);
     }
 }
