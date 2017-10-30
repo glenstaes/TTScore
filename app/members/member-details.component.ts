f4707c39621f8f08fd6aa56b00f851bd6e3d1bb6
Glen Staes <staeseke@gmail.com>
2017-10-30 14:47:54 +0100

    feat(members): Add overview of results grouped by ranking

--- app/members/member-details.component.ts
+++ app/members/member-details.component.ts
@@ -21,6 +21,7 @@ export class MemberDetailsComponent {
     member: ClubMember = undefined;
     memberId: number;
     isLoading: boolean = false;
+    resultsByRanking: Array<any> = [];
 
     /**
      * Creates a new instance of the component
@@ -39,8 +40,52 @@ export class MemberDetailsComponent {
     ngOnInit() {
         this._activatedRoute.params.subscribe((params) => {
             this.memberId = +params["uniqueIndex"];
-            this._loadFromTabT();
+            this._loadFromTabT().subscribe(() => {
+                this.calculateOverview();
+            });
         })
+    }
+
+    /**
+     * Calculates the overview of results grouped by the ranking
+     */
+    calculateOverview(){
+        this.resultsByRanking = [];
+
+        const resultsByRankingLocal = {};
+        this.member.resultEntries.forEach((resultEntry) => {
+            // Initialise a new entry if the ranking was not added yet
+            if(Object.keys(resultsByRankingLocal).indexOf(resultEntry.ranking) === -1){
+                resultsByRankingLocal[resultEntry.ranking] = {
+                    won: 0,
+                    loss: 0,
+                    matches: []
+                };
+            }
+
+            // Improve the won or loss number depending on the result
+            if(resultEntry.resultIndicator === "V"){
+                resultsByRankingLocal[resultEntry.ranking].won += 1;
+            } else {
+                resultsByRankingLocal[resultEntry.ranking].loss += 1;
+            }
+
+            resultsByRankingLocal[resultEntry.ranking].matches.push(resultEntry);
+        });
+
+        // Map to an array that can be shown
+        const rankingKeys = Object.keys(resultsByRankingLocal).sort();
+        for(let i = 0, len = rankingKeys.length; i < len; i++){
+            let currentRanking = rankingKeys[i];
+            let currentResults = resultsByRankingLocal[currentRanking];
+
+            this.resultsByRanking.push({
+                ranking: currentRanking,
+                won: currentResults.won,
+                loss: currentResults.loss,
+                matches: currentResults.matches
+            });
+        }
     }
 
     /**
@@ -52,6 +97,7 @@ export class MemberDetailsComponent {
         this.isLoading = true;
         this._loadFromTabT().subscribe(() => {
             this.isLoading = false;
+            this.calculateOverview();
         });
     }
 
@@ -67,12 +113,9 @@ export class MemberDetailsComponent {
      * Loads the data from the TabT api and displays the data.
      */
     private _loadFromTabT() {
-        const observable = this._clubMembersService.getFromTabT(this.memberId);
-        
-        observable.subscribe((importedMembers) => {
+        return this._clubMembersService.getFromTabT(this.memberId).map((importedMembers) => {
             this.member = importedMembers;
+            return this.member;
         });
-
-        return observable;
     }
 }
